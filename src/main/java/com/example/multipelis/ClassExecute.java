07dacd88595a39c047bec4b6c24077afb1790e83
CSebasT <107124936+CSebasT@users.noreply.github.com>
2023-06-07 00:57:23 -0500

    cambio

--- src/main/java/com/example/multipelis/ClassExecute.java
+++ src/main/java/com/example/multipelis/ClassExecute.java
@@ -3,5 +3,6 @@ package com.example.multipelis;
 public class ClassExecute {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Cambio");
     }
 }
